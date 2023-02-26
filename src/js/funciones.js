f030b81c9fb02f50d9dd0db68693ab4f46c38b4f
Flores-AM <agustinmatias62@gmail.com>
2023-02-26 16:30:16 -0300

    storage a darkmode

--- src/js/funciones.js
+++ src/js/funciones.js
@@ -85,5 +85,19 @@ function darkMode() {
 
   botonDarkMode.addEventListener("click", function () {
     document.body.classList.toggle("dark-mode");
+
+    // Guardar en localstorage
+    if (document.body.classList.contains("dark-mode")) {
+      localStorage.setItem("dark-mode", "true");
+    } else {
+      localStorage.setItem("dark-mode", "false");
+    }
   });
+}
+
+// Obtener el modo actual
+if (localStorage.getItem("dark-mode") === "true") {
+  document.body.classList.add("dark-mode");
+} else {
+  document.body.classList.remove("dark-mode");
 }
